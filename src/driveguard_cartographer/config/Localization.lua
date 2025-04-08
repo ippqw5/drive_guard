8d4c0b5fd127a036f22247fc2939b174d869a277
confiyoon <1760298581@qq.com>
2025-04-08 22:27:26 +0800

    update carto lua to use imu and odom (external)

--- src/driveguard_cartographer/config/Localization.lua
+++ src/driveguard_cartographer/config/Localization.lua
@@ -3,7 +3,5 @@ include "MySlam.lua"
 TRAJECTORY_BUILDER.pure_localization_trimmer = {
   max_submaps_to_keep = 3,  -- 最大保存子图数，纯定位模式通过子图进行定位
 }
-POSE_GRAPH.optimize_every_n_nodes = 20  -- 每20个有效帧一个子图，子图构建完成要闭环检测一次，该数越小，闭环检测越频繁
 
- 
 return options
